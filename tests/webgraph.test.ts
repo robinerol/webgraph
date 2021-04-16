a917ab135a1c7b073ed473436e2f01e7bf8d450a
Robin Stuedle <robin.stuedle@uni-ulm.de>
2021-04-16 11:33:42 +0200

    test: add tests for the missing methods of the webgraph class

--- tests/webgraph.test.ts
+++ tests/webgraph.test.ts
@@ -5,9 +5,12 @@ import { WebGraph } from "../src";
 import {
   DEFAULT_GRAPH_CONFIGURATION,
   Layout,
+  NodeType,
 } from "../src/Configuration/index";
 import { IGraphConfiguration } from "../types/Configuration";
 import { WebGLRenderer } from "sigma";
+import { SerializedEdge, SerializedNode } from "graphology-types";
+//import { ActionType } from "../src/WebGraph/History";
 
 jest.mock("sigma", () => {
   return {
@@ -38,9 +41,28 @@ jest.mock("sigma", () => {
         kill: jest.fn().mockImplementation(() => {
           return {};
         }),
+        process: jest.fn().mockImplementation(() => {
+          return {};
+        }),
+        refresh: jest.fn().mockImplementation(() => {
+          return {};
+        }),
+        settings: jest.fn().mockImplementation(() => {
+          return {
+            defaultNodeType: jest.fn().mockImplementation(() => {
+              return {};
+            }),
+          };
+        }),
       };
     }),
     AbstractNodeProgram: jest.fn().mockImplementation(() => {
+      return {};
+    }),
+    easings: jest.fn().mockImplementation(() => {
+      return {};
+    }),
+    animateNodes: jest.fn().mockImplementation(() => {
       return {};
     }),
   };
@@ -218,107 +240,515 @@ describe("test public methods of the WebGraph class", () => {
   });
 
   describe("graph manipulation", () => {
-    const graph = new Graph();
+    let graph: Graph;
+    let webGraph: WebGraph;
 
-    beforeAll(() => {
+    const testNode1 = {
+      key: "n1",
+    };
+
+    const testNode2 = {
+      key: "n2",
+    };
+
+    const testNode3 = {
+      key: "n3",
+    };
+
+    const testEdge = {
+      key: "testEdge",
+      source: "n1",
+      target: "n2",
+    };
+
+    beforeEach(() => {
       const container = document.createElement("div");
+      graph = new Graph();
 
-      const webGraph = new WebGraph(container, graph);
+      graph.addNode(testNode1.key);
+      graph.addNode(testNode2.key);
+      graph.addNode(testNode3.key);
+      graph.addEdgeWithKey(testEdge.key, testEdge.source, testEdge.target);
+
+      webGraph = new WebGraph(container, graph);
       webGraph.render();
     });
 
-    it("should merge edges", () => {
-      //TODO
-      expect(true).toBeTruthy();
+    it("should overwrite edge attributes/merge edge", () => {
+      const edges = new Set<SerializedEdge>();
+
+      const edge = {
+        key: testEdge.key,
+        source: testEdge.source,
+        target: testEdge.target,
+        attributes: {
+          abc: "556677abc",
+        },
+      };
+
+      edges.add(edge);
+
+      const exportedGraphBefore = webGraph.exportGraph();
+
+      expect(exportedGraphBefore.edges).not.toContainEqual(edge);
+
+      webGraph.mergeEdges(edges);
+
+      const exportedGraphAfter = webGraph.exportGraph();
+
+      expect(exportedGraphAfter.edges).toContainEqual(edge);
+      expect(exportedGraphAfter.edges).toHaveLength(1);
     });
 
-    it("should add edges", () => {
-      //TODO
-      expect(true).toBeTruthy();
+    it("should add edge", () => {
+      const edges = new Set<SerializedEdge>();
+
+      const edge1 = {
+        key: testEdge.key,
+        source: testEdge.source,
+        target: testEdge.target,
+        attributes: {
+          abc: "556677abc",
+        },
+      };
+
+      const edge2 = {
+        key: "iAnSnEn72",
+        source: "n2",
+        target: "n3",
+      };
+
+      edges.add(edge1);
+      edges.add(edge2);
+
+      const exportedGraphBefore = webGraph.exportGraph();
+
+      expect(exportedGraphBefore.edges).not.toContainEqual(edge1);
+      expect(exportedGraphBefore.edges).not.toContainEqual(edge2);
+
+      webGraph.mergeEdges(edges);
+
+      const exportedGraphAfter = webGraph.exportGraph();
+
+      expect(exportedGraphAfter.edges).toContainEqual(edge1);
+      expect(exportedGraphAfter.edges).toContainEqual(edge2);
+      expect(exportedGraphAfter.edges).toHaveLength(2);
     });
 
-    it("should overwrite edge attributes", () => {
-      //TODO
-      expect(true).toBeTruthy();
+    it("should replace edges", () => {
+      const edges = new Set<SerializedEdge>();
+
+      const edge1 = {
+        key: "dU3Js(k9",
+        source: "n2",
+        target: "n1",
+      };
+
+      const edge2 = {
+        key: "iAnSnEn72",
+        source: "n2",
+        target: "n3",
+      };
+
+      edges.add(edge1);
+      edges.add(edge2);
+
+      const exportedGraphBefore = webGraph.exportGraph();
+
+      expect(exportedGraphBefore.edges).not.toContainEqual(edge1);
+      expect(exportedGraphBefore.edges).not.toContainEqual(edge2);
+
+      webGraph.replaceEdges(edges);
+
+      const exportedGraphAfter = webGraph.exportGraph();
+
+      expect(exportedGraphAfter.edges).toContainEqual(edge1);
+      expect(exportedGraphAfter.edges).toContainEqual(edge2);
+      expect(exportedGraphAfter.edges).not.toContainEqual(testEdge);
+      expect(exportedGraphAfter.edges).toHaveLength(2);
     });
 
-    it("should merge nodes", () => {
-      //TODO
-      expect(true).toBeTruthy();
+    it("should overwrite node attributes/merge node", () => {
+      const nodes = new Array<SerializedNode>();
+
+      const node = {
+        key: testNode1.key,
+        attributes: {
+          abc: "556677abc",
+        },
+      };
+
+      nodes.push(node);
+
+      const exportedGraphBefore = webGraph.exportGraph();
+
+      expect(exportedGraphBefore.nodes).not.toContainEqual(node);
+
+      webGraph.mergeNodes(nodes);
+
+      const exportedGraphAfter = webGraph.exportGraph();
+
+      expect(exportedGraphAfter.nodes).toContainEqual(node);
+      expect(exportedGraphAfter.nodes).toHaveLength(3);
     });
 
     it("should drop nodes", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      const nodes = new Array<SerializedNode>();
+
+      const node = {
+        key: "su3ds9dja",
+        attributes: {
+          abc: "3867387643",
+        },
+      };
+
+      nodes.push(node);
+      nodes.push(testNode1);
+      nodes.push(testNode3);
+
+      const exportedGraphBefore = webGraph.exportGraph();
+
+      expect(exportedGraphBefore.nodes).not.toContainEqual(node);
+      expect(exportedGraphBefore.nodes).toContainEqual(testNode1);
+      expect(exportedGraphBefore.nodes).toContainEqual(testNode3);
+      expect(exportedGraphBefore.nodes).toHaveLength(3);
+
+      webGraph.dropNodes(nodes);
+
+      const exportedGraphAfter = webGraph.exportGraph();
+
+      expect(exportedGraphAfter.nodes).not.toContainEqual(node);
+      expect(exportedGraphAfter.nodes).not.toContainEqual(testNode1);
+      expect(exportedGraphAfter.nodes).not.toContainEqual(testNode3);
+      expect(exportedGraphAfter.nodes).toHaveLength(1);
     });
 
-    it("should add nodes", () => {
-      //TODO
-      expect(true).toBeTruthy();
-    });
+    it("should add node", () => {
+      const nodes = new Array<SerializedNode>();
 
-    it("should overwrite node attributes", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      const node2 = {
+        key: "su3ds9dja",
+        attributes: {
+          abc: "3867387643",
+        },
+      };
+
+      nodes.push(node2);
+
+      const exportedGraphBefore = webGraph.exportGraph();
+
+      expect(exportedGraphBefore.nodes).not.toContainEqual(node2);
+      expect(exportedGraphBefore.nodes).toHaveLength(3);
+
+      webGraph.mergeNodes(nodes);
+
+      const exportedGraphAfter = webGraph.exportGraph();
+
+      expect(exportedGraphAfter.nodes).toContainEqual(node2);
+      expect(exportedGraphAfter.nodes).toHaveLength(4);
     });
   });
 
   describe("layout and render", () => {
-    it("should toggle edge rendering", () => {
-      //TODO
-      expect(true).toBeTruthy();
-    });
+    let webGraph: WebGraph;
+    const defaultLayout = Layout.RANDOM;
+    const defaultLayoutConfig = {};
+    const defaultNodeType = NodeType.RECTANGLE;
 
-    it("should toggle rendering just important edges", () => {
-      //TODO
-      expect(true).toBeTruthy();
-    });
+    beforeEach(() => {
+      const container = document.createElement("div");
+      const graph = new Graph();
 
-    it("should reapply layout", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      const testNode1 = {
+        key: "n1",
+      };
+
+      const testNode2 = {
+        key: "n2",
+      };
+
+      const testNode3 = {
+        key: "n3",
+      };
+
+      const testEdge = {
+        key: "testEdge",
+        source: "n1",
+        target: "n2",
+      };
+
+      graph.addNode(testNode1.key);
+      graph.addNode(testNode2.key);
+      graph.addNode(testNode3.key);
+      graph.addEdgeWithKey(testEdge.key, testEdge.source, testEdge.target);
+
+      webGraph = new WebGraph(container, graph, {
+        layout: defaultLayout,
+        layoutConfiguration: defaultLayoutConfig,
+        defaultNodeType: defaultNodeType,
+      });
+      webGraph.render();
     });
 
     it("should set and apply layout", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      expect(webGraph["configuration"].layout).toEqual(defaultLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        defaultLayoutConfig
+      );
+
+      const newLayout = Layout.CIRCULAR;
+      const newLayoutConfig = { circularLayoutOptions: {} };
+
+      webGraph.setAndApplyLayout(newLayout, newLayoutConfig);
+
+      expect(webGraph["configuration"].layout).toEqual(newLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        newLayoutConfig
+      );
+    });
+
+    it("should reapply layout", () => {
+      expect(webGraph["configuration"].layout).toEqual(defaultLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        defaultLayoutConfig
+      );
+
+      webGraph.reapplyLayout();
+
+      expect(webGraph["configuration"].layout).toEqual(defaultLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        defaultLayoutConfig
+      );
+    });
+
+    it("should reapply layout force atlas 2 and overwrite preAppliedLayout", () => {
+      expect(webGraph["configuration"].layout).toEqual(defaultLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        defaultLayoutConfig
+      );
+
+      const newLayout = Layout.FORCEATLAS2;
+      const newLayoutConfig = {
+        forceAtlas2LayoutOptions: {
+          iterations: 5,
+          preAppliedLayout: Layout.RANDOM,
+        },
+      };
+
+      webGraph.setAndApplyLayout(newLayout, newLayoutConfig);
+
+      expect(webGraph["configuration"].layout).toEqual(newLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        newLayoutConfig
+      );
+
+      webGraph.reapplyLayout();
+
+      const expectedLayoutOptions = {
+        forceAtlas2LayoutOptions: {
+          iterations: 5,
+          preAppliedLayout: undefined,
+        },
+      };
+
+      expect(webGraph["configuration"].layout).toEqual(newLayout);
+      expect(webGraph["configuration"].layoutConfiguration).toEqual(
+        expectedLayoutOptions
+      );
     });
 
     it("should set and apply default node type", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      expect(webGraph["configuration"].defaultNodeType).toEqual(
+        defaultNodeType
+      );
+
+      const newType = NodeType.RING;
+
+      webGraph.setAndApplyDefaultNodeType(newType);
+
+      expect(webGraph["configuration"].defaultNodeType).toEqual(newType);
     });
   });
 
   describe("history", () => {
     it("should enable history", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      const container = document.createElement("div");
+      const webGraph = new WebGraph(container, new Graph(), {
+        enableHistory: true,
+      });
+      webGraph.render();
+
+      expect(webGraph["isHistoryEnabled"]).toBeTruthy();
+      expect(webGraph["history"]).toBeDefined();
     });
 
     it("should disabled history", () => {
-      //TODO
-      expect(true).toBeTruthy();
+      const container = document.createElement("div");
+      const webGraph = new WebGraph(container, new Graph(), {
+        enableHistory: false,
+      });
+      webGraph.render();
+
+      expect(webGraph["isHistoryEnabled"]).toBeFalsy();
+      expect(webGraph["history"]).toBeUndefined();
     });
 
-    it("should undo latest action", () => {
-      //TODO
-      expect(true).toBeTruthy();
-    });
+    describe("history actions", () => {
+      let webGraph: WebGraph;
 
-    it("should undo last two actions", () => {
-      //TODO
-      expect(true).toBeTruthy();
-    });
+      const layout = Layout.RANDOM;
+      const layoutConfig = { randomLayoutOptions: {} };
+      const nodeType = NodeType.RECTANGLE;
+      const nodeTypeTwo = NodeType.CIRCLE;
 
-    it("should redo latest undone action", () => {
-      //TODO
-      expect(true).toBeTruthy();
-    });
+      beforeEach(() => {
+        const container = document.createElement("div");
+        webGraph = new WebGraph(container, new Graph(), {
+          enableHistory: true,
+        });
 
-    it("should undo two then redo one action", () => {
-      //TODO
-      expect(true).toBeTruthy();
+        webGraph.render();
+
+        expect(webGraph["isHistoryEnabled"]).toBeTruthy();
+        expect(webGraph["history"]).toBeDefined();
+
+        webGraph.setAndApplyLayout(layout, layoutConfig);
+        webGraph.setAndApplyDefaultNodeType(nodeType);
+        webGraph.setAndApplyDefaultNodeType(nodeTypeTwo);
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeTypeTwo);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+      });
+
+      it("should undo latest action", () => {
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeType);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+      });
+
+      it("should undo last two actions", () => {
+        webGraph.undo();
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.defaultNodeType
+        );
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+      });
+
+      it("should redo latest undone action", () => {
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeType);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.redo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeTypeTwo);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+      });
+
+      it("should undo two then redo one action", () => {
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeType);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.defaultNodeType
+        );
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.redo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeType);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+      });
+
+      it("should undo three then redo three actions", () => {
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeType);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.defaultNodeType
+        );
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.undo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.defaultNodeType
+        );
+        expect(webGraph["configuration"].layout).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.layout
+        );
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.layoutConfiguration
+        );
+
+        webGraph.redo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(
+          DEFAULT_GRAPH_CONFIGURATION.defaultNodeType
+        );
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.redo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeType);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+
+        webGraph.redo();
+
+        expect(webGraph["configuration"].defaultNodeType).toEqual(nodeTypeTwo);
+        expect(webGraph["configuration"].layout).toEqual(layout);
+        expect(webGraph["configuration"].layoutConfiguration).toEqual(
+          layoutConfig
+        );
+      });
     });
   });
 });
